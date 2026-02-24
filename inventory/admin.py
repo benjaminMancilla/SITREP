29e38292d5555cd7b434e91687d7ebd3d2ffc2a9
Benjamin Mancilla <benjamin.mancilla@ug.uchile.cl>
2026-02-24 02:00:04 -0300

    feat: adding Django admin panel

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import (
+    Naviera, Usuario, Nave, Tripulacion, 
+    Proposito, Periodicidad, Recurso, 
+    MatrizNaveRecurso, PeriodoRevision, FichaRegistro
+)
 
-# Register your models here.
+@admin.register(Usuario)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Contexto Multi-Tenant', {'fields': ('naviera', 'rut', 'rol')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Contexto Multi-Tenant', {'fields': ('naviera', 'rut', 'rol')}),
+    )
+    
+    list_display = ('rut', 'username', 'naviera', 'rol', 'is_active')
+    search_fields = ('rut', 'username', 'email')
+    list_filter = ('naviera', 'rol', 'is_active')
+
+admin.site.register(Naviera)
+admin.site.register(Nave)
+admin.site.register(Tripulacion)
+admin.site.register(Proposito)
+admin.site.register(Periodicidad)
+admin.site.register(Recurso)
+admin.site.register(MatrizNaveRecurso)
+admin.site.register(PeriodoRevision)
+admin.site.register(FichaRegistro)
