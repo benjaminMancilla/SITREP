3d8d368205dcd93426a0f30c8e322605721294a8
Benjamin Mancilla <benjamin.mancilla@ug.uchile.cl>
2026-07-03 14:46:16 -0400

    fix: adding health check and custom error screen for error 500.

--- core/urls.py
+++ core/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 
 from . import views as views_core
 
 urlpatterns = [
+    path("health/", lambda r: HttpResponse("ok"), name="health"),
     path("", views_core.homepage, name="homepage"),
     path("admin/", admin.site.urls),
     path("<slug:slug>/", include("sitrep.inspection.urls", namespace="inventory")),
